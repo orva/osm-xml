4c49a5d0b0b949f372b22d7177c0ea47a8f335c7
Felix Schütt <felix.schuett@maps4print.com>
2017-06-16 01:15:08 +0200

    Added std::Error for Error

--- src/error.rs
+++ src/error.rs
@@ -56,3 +56,13 @@ impl fmt::Display for Error {
         
     }
 }
+
+impl ::std::error::Error for Error {
+    fn description(&self) -> &str {
+        "OSM XML error"
+    }
+
+    fn cause(&self) -> Option<&::std::error::Error> {
+        None
+    }
+}
